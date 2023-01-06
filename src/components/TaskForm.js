7ed3b917293aacae9e75c2b78c43fd493b5dc448
LucianoRomero1 <lucianooromero1@gmail.com>
2023-01-06 11:15:37 -0300

    CRUD finish

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
 import { v4 as uuid } from "uuid";
-import { addTask } from "../features/tasks/taskSlice";
-
+import { addTask, editTask } from "../features/tasks/taskSlice";
 
 function TaskForm() {
-  const [task, setTask] = useState({});
+  const [task, setTask] = useState({
+    title: "",
+    description: ""
+  });
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+  const tasks = useSelector((state) => state.tasks);
 
   const handleChange = (e) => {
     setTask({
@@ -17,27 +23,50 @@ function TaskForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addTask({
-      ...task,
-      id: uuid()
-    }));
+
+    if(params.id){
+      dispatch(
+        editTask(task)
+      )
+    }else{
+      dispatch(
+        addTask({
+          ...task,
+          id: uuid(),
+        })
+      );
+    }
+  
+    navigate("/");
   };
 
+  useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
+    }
+  }, [params.id, tasks]);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+      <label htmlFor="title" className="block text-xs font-bold mb-1">Task:</label>
       <input
         name="title"
         type="text"
         placeholder="title"
         onChange={handleChange}
+        value={task.title}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       />
+      <label htmlFor="description" className="block text-xs font-bold mb-1">Description:</label>
       <textarea
         name="description"
         placeholder="description"
         onChange={handleChange}
+        value={task.description}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       />
 
-      <button>Save</button>
+      <button className="bg-indigo-600 px-2 py-1">Save</button>
     </form>
   );
 }
